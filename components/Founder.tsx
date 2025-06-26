567653c0feb2411a306618def4d6cfd90ff9a90a
justDenzil <vikash.denzil@gmail.com>
2025-06-27 02:38:58 +0530

    see how it works moda -v12

--- components/Founder.tsx
+++ components/Founder.tsx
@@ -2,7 +2,7 @@
 
 import React from "react"
 import Image from "next/image"
-import Link from "next/link"
+// import Link from "next/link"
 import { motion } from "framer-motion"
 import { fadeInUp } from "@/lib/animations"
 
